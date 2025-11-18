8089723770e6e388916de310d2e9c696a9fb5908
Basti070301 <sheo124538@gmail.com>
2025-11-18 16:01:58 +0100

    anpassung

--- Model/linear_regression/Skript/lineare_regression_wage_data.py
+++ Model/linear_regression/Skript/lineare_regression_wage_data.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
-#df = pd.read_csv("Model/linear_regression/Data/Raw/birthweight_data.csv")
-df = pd.read_csv('../Data/raw/birthweight_data.csv')
+df = pd.read_csv("Model/linear_regression/Data/Raw/birthweight_data.csv")
 
-df.describe()
+print(df.info())
